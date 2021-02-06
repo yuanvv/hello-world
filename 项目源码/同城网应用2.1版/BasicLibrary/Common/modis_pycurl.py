cfa1cf4c1bb102a2be6bd2562e9389d933b39e5e
yuanvv <62780315+yuanvv@users.noreply.github.com>
2021-02-06 11:49:52 +0800

    Add files via upload

---
+++ 项目源码/同城网应用2.1版/BasicLibrary/Common/modis_pycurl.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# 本代码中的用户名和密码不可用
+# 如需测试，请先在https://urs.earthdata.nasa.gov/home注册账号
+
+import os, re
+import pycurl
+import urllib.parse
+from io import BytesIO
+
+def get_modis_by_pycurl():
+    # 实例化pycurl，生成对象
+    c = pycurl.Curl()
+    c.unsetopt(pycurl.CUSTOMREQUEST)
+    c.setopt(pycurl.NOSIGNAL, 1)
+    
+    # 设置访问HTTPS
+    c.setopt(pycurl.SSL_VERIFYPEER, 0)
+    c.setopt(pycurl.SSL_VERIFYHOST, 0)
+    
+    # 是否允许重定向
+    c.setopt(pycurl.FOLLOWLOCATION, True)
+    
+    # 使用cookie文件记录登录信息
+    cookieFile = 'cookieFile.cookie'
+    if os.path.isfile(cookieFile):
+        os.remove(cookieFile)
+    
+    c.setopt(pycurl.COOKIEFILE, cookieFile)
+    c.setopt(pycurl.COOKIEJAR, cookieFile)
+    
+    # 设置服务端应答回调函数
+    c.resp_header_buf = BytesIO()
+    c.resp_body_buf = BytesIO()
+    c.setopt(pycurl.HEADERFUNCTION, c.resp_header_buf.write) # 设置服务端应答head时的回调函数
+    c.setopt(pycurl.WRITEFUNCTION, c.resp_body_buf.write) # 设置服务端应答body时的回调函数 
+    
+    # 取得登录页面
+    url = 'https://urs.earthdata.nasa.gov/home'
+    c.setopt(pycurl.HTTPGET, True)
+    c.setopt(pycurl.URL, url) 
+    c.perform()
+    body = c.resp_body_buf.getvalue()
+    
+    # 解析取得token
+    pt = re.compile(r'.*<input type="hidden" name="authenticity_token" value="(.*)" />.*')    
+    print(len(body))
+    token = pt.findall(body.decode('gbk'))[0]
+    print('token: ', token)
+    
+    url = 'https://urs.earthdata.nasa.gov/login'
+    forms = {
+        "username": "xufive",
+        "password": "********",
+        "redirect_uri": "",
+        "commit": "Log+in",
+        "client_id": "",
+        "authenticity_token": token
+    }
+    c.setopt(pycurl.POSTFIELDS, urllib.parse.urlencode(forms))
+    c.setopt(pycurl.POST, True)
+    c.setopt(pycurl.URL, url)
+    c.perform()
+    print('login: ', 'OK')
+    
+    # 设置服务端应答回调函数
+    c.resp_header_buf = BytesIO() # 设置访问HTTPS
+    c.resp_body_buf = BytesIO() # 设置访问HTTPS
+    c.setopt(pycurl.HEADERFUNCTION, c.resp_header_buf.write) # 设置服务端应答head时的回调函数
+    c.setopt(pycurl.WRITEFUNCTION, c.resp_body_buf.write) # 设置服务端应答body时的回调函数
+    
+    # 数据文件下载地址
+    url = 'https://search.earthdata.nasa.gov/search?m=-0.0703125!0!2!1!0!0%2C2&fl=2%20-%20Geophys.%20Variables%2C%20Sensor%20Coordinates&gdf=NetCDF'
+    
+    # 第一次访问下载地址，返回页面中有验证链接
+    c.setopt(pycurl.HTTPGET, True)
+    c.setopt(pycurl.URL, url)
+    c.perform()
+    body = c.resp_body_buf.getvalue()
+    
+    # 解析内容，取得验证用户的链接
+    pu = re.compile(r'href="(https://ladsweb.modaps.eosdis.nasa.gov/search/)"')        
+    urls = pu.findall(body.decode('gbk'))
+    temp_url = urls[0].replace('&amp;', '&')
+    print ('furl: ', temp_url)
+    
+    c.setopt(pycurl.FOLLOWLOCATION, False) # 不允许重定向
+    c.setopt(pycurl.HTTPGET, True)
+    c.setopt(pycurl.URL, temp_url)
+    c.perform()
+    print (c.getinfo(pycurl.HTTP_CODE))
+    
+    # 设置服务端应答回调函数
+    c.resp_header_buf = BytesIO()# 设置访问HTTPS
+    c.resp_body_buf = BytesIO()# 设置访问HTTPS
+    c.setopt(pycurl.HEADERFUNCTION, c.resp_header_buf.write) # 设置服务端应答head时的回调函数
+    c.setopt(pycurl.WRITEFUNCTION, c.resp_body_buf.write) # 设置服务端应答body时的回调函数
+    
+    # 下载
+    c.setopt(pycurl.HTTPGET, True)
+    c.setopt(pycurl.URL, url)
+    f = open('fb/modis_pycurl.hdf', 'wb')
+    c.setopt(pycurl.WRITEDATA, f) # 应答为文件时，指定流资源
+    c.perform()
+    c.close()
+    print('OK')
+
+if __name__ == '__main__':
+    get_modis_by_pycurl()
+ 
